5329c8f7375ee9b187c4399a72b694bdbfebb9b9
karianmash <ianmachariak17@gmail.com>
2022-08-01 00:02:17 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+var addBtn = document.getElementById("add-btn");
+var closeBtn = document.getElementById("close-btn");
+addBtn === null || addBtn === void 0 ? void 0 : addBtn.addEventListener("click", function (e) {
+    alert("Hi");
+});
---
+++ index.ts
@@ -0,0 +1,6 @@
+let addBtn = document.getElementById("add-btn") as HTMLElement;
+let closeBtn = document.getElementById("close-btn") as HTMLElement;
+
+addBtn?.addEventListener("click", (e) => {
+    alert("Hi")
+});
